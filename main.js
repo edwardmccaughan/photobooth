f9654744a12dfafcb259dcbfdebd9d37f9a0d16b
edward mccaughan <edwardmccaughan@gmail.com>
2019-04-18 16:38:43 +0200

    bit of tidying

--- main.js
+++ main.js
@@ -3,56 +3,58 @@ const exec = require("child_process").exec
 const fs = require('fs');
 
 function createWindow () {
-  // Create the browser window.
   let win = new BrowserWindow({ width: 800, height: 600 })
 
-  // and load the index.html of the app.
   win.loadFile('index.html')
-  win.setFullScreen(true)
+  // win.setFullScreen(true)
 
-  function pdfSettings() {
-      var paperSizeArray = ["A4", "A5"];
-      var option = {
-          landscape: false,
-          marginsType: 0,
-          printBackground: false,
-          printSelectionOnly: false,
-          // pageSize: paperSizeArray[settingCache.getPrintPaperSize()-1],
-      };
-    return option;
-  }
-
-  function print_to_pdf() {
-    win.webContents.printToPDF(pdfSettings(), function(err, data) {
-      if (err) {
-        console.log('error!', err)
-        return;
-      }
-      try{
-          const filePath = './generated_pdf.pdf'
-          fs.writeFileSync(filePath, data);
-
-          exec(`lpr ${filePath}`, () => fs.unlink(filePath, (error) => {console.log(error)}));
-
-      }catch(err){
-        console.log('big error!', err)
-      }
-    })
-  }
-
-
-  // const ipc = electron.ipcMain
-  ipcMain.on('print', function (event) {
+  ipcMain.on('print_photo', function (event) {
     console.log('print event received')
-    // console.log(win.webContents.getPrinters())
-    // win.webContents.print({silent: false, deviceName: 'ML-1630-Series'}, (success) => {
-    // win.webContents.print({silent: true})
-    // win.webContents.print({silent: true}, (success) => {
-    //   console.log('printing: ', success);
-    // });
-
-    print_to_pdf()
+    // printer.print_to_pdf(win)
   })
 }
+
+// printer = {
+//   pdfSettings: function() {
+//       var paperSizeArray = ["A4", "A5"];
+//       var option = {
+//           landscape: false,
+//           marginsType: 0,
+//           printBackground: false,
+//           printSelectionOnly: false,
+//           // pageSize: paperSizeArray[settingCache.getPrintPaperSize()-1],
+//       };
+//     return option;
+//   },
+
+//   print_to_pdf: function(win) {
+//     console.log('printing!')
+//     return
+
+
+//     win.webContents.printToPDF(printer.pdfSettings(), function(err, data) {
+      
+//       if (err) {
+//         console.log('error!', err)
+//         return;
+//       }
+//       try{
+          
+//           const filePath = './generated_pdf.pdf'
+//           fs.writeFileSync(filePath, data);
+
+//           exec(`lpr ${filePath}`, () => fs.unlink(filePath, (error) => {console.log(error)}));
+
+//       }catch(err){
+//         console.log('big error!', err)
+//       }
+//     })
+//   }
+// }
+
+
+
+
+
 
 app.on('ready', createWindow)
--- src/capture.js
+++ src/capture.js
@@ -1,39 +1,21 @@
+const {ipcRenderer} = require('electron')
+
 class WebcamCapture {
   constructor() {
     // The width and height of the captured photo. We will set the
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
-
     this.width = 320;    // We will scale the photo width to this
     this.height = 0;     // This will be computed based on the input stream
 
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
     this.streaming = false;
-
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
 
     this.video = null;
     this.canvas = null;
     this.photo = null;
-    this.startbutton = null;
+    this.startbutton = null; 
 
-    const ipc = require('electron').ipcRenderer
-
-    var keydown_callback = function(event) {
-      console.log(event.code)
-      if(event.code == "KeyQ") {
-        takepicture()
-      } else if(event.code == "KeyW") {
-        console.log('trying to print')
-        ipc.send('print')
-      }
-    }
-
-
-    window.addEventListener("keydown", keydown_callback , false)
-
+    window.addEventListener("keydown", this.keydown_handler.bind(this) , false)
   }
 
   startup() {
@@ -53,15 +35,7 @@ class WebcamCapture {
 
     video.addEventListener('canplay', function(ev){
       if (!this.streaming) {
-        this.height = this.video.videoHeight / (this.video.videoWidth/ this.width);
-      
-        // Firefox currently has a bug where the height can't be read from
-        // the video, so we will make assumptions if this happens.
-      
-        if (isNaN(this.height)) {
-          this.height = this.width / (4/3);
-        }
-      
+        this.height = this.video.videoHeight / (this.video.videoWidth/ this.width);     
         this.video.setAttribute('width', this.width);
         this.video.setAttribute('height', this.height);
         this.canvas.setAttribute('width', this.width);
@@ -78,8 +52,6 @@ class WebcamCapture {
     this.clearphoto();
   }
 
-  // Fill the photo with an indication that none has been
-  // captured.
   clearphoto() {
     var context = canvas.getContext('2d');
     context.fillStyle = "#AAA";
@@ -108,12 +80,29 @@ class WebcamCapture {
     }
   }
 
-  
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  // window.addEventListener('load', startup, false);
+  print_photo() {
+    ipcRenderer.send('print_photo')
+  }
+
+  keydown_handler(event) {
+    console.log('key pressed', event.code)
+    if(event.code == "KeyQ") {
+      this.takepicture()
+    } else if(event.code == "KeyW") {
+      console.log('trying to print')
+      this.print_photo()
+    }
+  }
 }
 
 
-window.webcam = new WebcamCapture()
-window.webcam.startup()
+
+// Set up our event listener to run the startup process
+// once loading is complete.
+window.addEventListener('load', function(){
+  window.webcam = new WebcamCapture()
+  window.webcam.startup()
+}, false);
+
+
+
